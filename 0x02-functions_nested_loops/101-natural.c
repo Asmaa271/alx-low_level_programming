5fa57b0a8826bf5f9b88e9c3bd5c3ca2438f55e6
Asmaa271 <aitelhajasmaa6@gmail.com>
2023-07-18 17:08:25 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,6 +13,6 @@ for (i = 0; i < 1024; i++)
 if ((i % 3) == 0 || (i % 5) == 0)
 sum += i;
 }
-printf("The sum of multiples of 3 or 5 below 1024 is: %d\n", sum);
+printf("%d\n", sum);
 return (0);
 }
