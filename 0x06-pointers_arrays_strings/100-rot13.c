c16299bcfd68e63a147f0c28abbb049c1321ff55
Asmaa271 <aitelhajasmaa6@gmail.com>
2023-07-27 13:43:49 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,10 +7,10 @@
  */
 char *rot13(char *str)
 {
-int i;
-int j;
-char t1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char t2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char *ptr = str;
+int i, j;
+char t1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char t2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 for (i = 0; str[i] != '\0'; i++)
 {
 for (j = 0; j < 52; j++)
@@ -18,8 +18,9 @@ for (j = 0; j < 52; j++)
 if (str[i] == t1[j])
 {
 str[i] = t2[j];
+break;
 }
 }
 }
-return (str);
+return (ptr);
 }
