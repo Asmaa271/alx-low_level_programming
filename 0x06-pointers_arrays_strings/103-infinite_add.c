62b84c3eb549db4c1e59afa4ee193c699fa9772d
Asmaa271 <aitelhajasmaa6@gmail.com>
2023-07-27 17:19:04 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,13 +3,13 @@
  * infinite_add - adds two numbers
  * @n1: number one
  * @n2: number two
- * @r: buffer 
- * @size_r: buffer size:
+ * @r: buffer
+ * @size_r: buffer size
  * Return: result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i = 0, j = 0, k, l, m, o , a = 0;
+int i = 0, j = 0, k, l, m, o, a = 0;
 while (*(n1 + i) != '\0')
 i++;
 while (*(n2 + j) != '\0')
@@ -22,7 +22,7 @@ if (size_r <= l + 1)
 return (0);
 r[l + 1] = '\0';
 i--, j--, size_r--;
-m = *(n1 + i) - 48, o = *(n2 + j) - 48;
+m = *(n1 + i)-48, o = *(n2 + j)-48;
 while (l >= 0)
 {
 k = m + o + a;
@@ -35,11 +35,11 @@ if (k > 0)
 else
 *(r + l) = '0';
 if (i > 0)
-i--, m = *(n1 + i) - 48;
+i--, m = *(n1 + i)-48;
 else
 m = 0;
-if (c2 > 0)
-c2--, o = *(n2 + c2) - 48;
+if (j > 0)
+j--, o = *(n2 + j)-48;
 else
 o = 0;
 l--, size_r--;
