9713180d9179100dc87698896f4de568861dff97
Asmaa271 <aitelhajasmaa6@gmail.com>
2023-08-17 09:08:39 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,30 +12,34 @@ void print_all(const char * const format, ...)
 va_list args;
 unsigned int i = 0;
 char *separator = "";
+char c;
 va_start(args, format);
 while (format && format[i])
 {
-switch (format[i])
+c = format[i];
+if (c == 'c')
 {
-case 'c':
 printf("%s%c", separator, va_arg(args, int));
-break;
-case 'i':
+separator = ", ";
+}
+else if (c == 'i')
+{
 printf("%s%d", separator, va_arg(args, int));
-break;
-case 'f':
+separator = ", ";
+}
+else if (c == 'f')
+{
 printf("%s%f", separator, va_arg(args, double));
-break;
-case 's':
+separator = ", ";
+}
+else if (c == 's')
 {
 char *str = va_arg(args, char *);
 if (str == NULL)
 str = "(nil)";
 printf("%s%s", separator, str);
-}
-break;
-}
 separator = ", ";
+}
 i++;
 }
 va_end(args);
