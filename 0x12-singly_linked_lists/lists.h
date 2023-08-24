b4d91ae5af421991977d5f3e21cc635e427feb70
Asmaa271 <aitelhajasmaa6@gmail.com>
2023-08-24 12:27:08 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>  
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -18,6 +18,4 @@ struct list_s *next;
 /* Function prototype */
 int _putchar(char c);
 size_t print_list(const list_t *h);
-
-
 #endif /* LISTS_H */
