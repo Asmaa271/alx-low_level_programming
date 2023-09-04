1ba3efd2e71565a662251285b46c943d90881972
Asmaa271 <aitelhajasmaa6@gmail.com>
2023-09-04 16:31:03 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,38 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include "main.h"
-#define BUFFER_SIZE 1024
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 /**
- * copy_file - function that copies the content of one file to another
- * @src_file: name of the source file
- * @dest_file: name of the destination file
- * Return: result
+ * error_exit - function that handles errors
+ * @code: the exit code
+ * @message: the error message to print
+ * Return : result
  */
-int copy_file(const char *src_file, const char *dest_file)
+void error_exit(int code, const char *message)
 {
-int src_fd;
-int dest_fd;
-char *buffer;
-int bytes_read, bytes_written;
-while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+dprintf(STDERR_FILENO, "%s\n", message);
+exit(code);
+}
+/**
+ * main - function handles file copyin
+ * @argc: number
+ * @argv: array
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
 {
-bytes_written = write(dest_fd, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
+if (argc != 3)
 {
-dprintf(2, "Error: Can't write to %s\n", dest_file);
-close(src_fd);
-close(dest_fd);
-return (99);
+error_exit(97, "Usage: cp file_from file_to");
+}
+const char *file_from = argv[1];
+const char *file_to = argv[2];
+int fd_from = open(file_from, O_RDONLY);
+if (fd_from == -1)
+{
+error_exit(98, "Error: Can't read from file");
+}
+int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
+S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+if (fd_to == -1)
+{
+error_exit(99, "Error: Can't write to file");
+}
+char buffer[1024];
+ssize_t bytes_read;
+while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+{
+ssize_t bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written == -1)
+{
+error_exit(99, "Error: Can't write to file");
 }
 }
 if (bytes_read == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", src_file);
-close(src_fd);
-close(dest_fd);
-return (98);
+error_exit(98, "Error: Can't read from file");
+}
+if (close(fd_from) == -1)
+{
+error_exit(100, "Error: Can't close fd");
+}
+if (close(fd_to) == -1)
+{
+error_exit(100, "Error: Can't close fd");
 }
 return (0);
 }
