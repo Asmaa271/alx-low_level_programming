0d7b72e3b3b9171876bd441c893b186bccaad146
Asmaa271 <aitelhajasmaa6@gmail.com>
2023-09-04 16:48:44 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <unistd.h>
+#define BUFFER_SIZE 1024
 /**
  * error_exit - function that handles errors
+ *
+ * @message: the error message
  * @code: the exit code
- * @message: the error message to print
- * Return : result
  */
-void error_exit(int code, const char *message)
+void error_exit(const char *message, int code)
 {
-dprintf(STDERR_FILENO, "%s\n", message);
+dprintf(2, "%s\n", message);
 exit(code);
 }
 /**
- * main - function handles file copyin
- * @argc: number
- * @argv: array
- * Return: Always 0
+ * copy_file - function that copies the content of one file to another
+ *
+ * @fd_from: the source file
+ * @fd_to: the destination file
+ *
+ * Return: result
  */
-int main(int argc, char *argv[])
+ssize_t copy_file(int fd_from, int fd_to)
 {
-if (argc != 3)
+char buffer[BUFFER_SIZE];
+ssize_t total_bytes_written = 0;
+while (1)
 {
-error_exit(97, "Usage: cp file_from file_to");
-}
-const char *file_from = argv[1];
-const char *file_to = argv[2];
-int fd_from = open(file_from, O_RDONLY);
-if (fd_from == -1)
-{
-error_exit(98, "Error: Can't read from file");
-}
-int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
-S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (fd_to == -1)
-{
-error_exit(99, "Error: Can't write to file");
-}
-char buffer[1024];
-ssize_t bytes_read;
-while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-{
-ssize_t bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1)
-{
-error_exit(99, "Error: Can't write to file");
-}
-}
+ssize_t bytes_read = read(fd_from, buffer, BUFFER_SIZE);
 if (bytes_read == -1)
 {
-error_exit(98, "Error: Can't read from file");
+return (-1);
 }
-if (close(fd_from) == -1)
+if (bytes_read == 0)
 {
-error_exit(100, "Error: Can't close fd");
+break;
 }
-if (close(fd_to) == -1)
+ssize_t bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
 {
-error_exit(100, "Error: Can't close fd");
+return (-1);
 }
-return (0);
+total_bytes_written += bytes_written;
+}
+return (total_bytes_written);
 }
